afd5c022295a35c7329047450971b38261ba7e57
nisaupb <116271847+nisaupb@users.noreply.github.com>
2022-12-28 20:23:37 +0700

    Add files via upload

---
+++ txt = 'Hello, nama saya john, dan umur s.py
@@ -0,0 +1,8 @@
+print("Nama     : annisa fitriani")
+print("Kelas    : TI.22.C1")
+print("NIM      : 312210023")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+umur = 24
+txt = "\nHello, Nama Saya Kjohn, dan umur saya adalah {0} tahun\n"
+
+print(txt.format(umur)) 
